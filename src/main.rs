e1ed17790f655ee9751b0cc056c98869659cdf38
Justin Thein <jthein@berkeley.edu>
2020-05-25 21:14:39 -0700

    change border

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,20 @@ const NRIGHT: u64 = 0xFEFEFEFEFEFEFEFE; // ~RIGHT
 
 /// Reproduction of an 8x8 game of life in Java (Browne and Tavener, 2012b).
 fn main() {
-    print_board(LEFT);
+    print_board(NLEFT);
 }
 
 /// Take a u64 and return a human readable 8x8 grid of its bits.
 fn print_board(int: u64) {
     let mut string_repr = format!("{:064b}", int);
-    let border = "▒ ▒ ▒ ▒ ▒ ▒ ▒ ▒ ▒ ▒";
+    let border = "+-----------------+";
     println!("{}", border);
     for i in (0..64).step_by(8) {
         let mut eight_chars = String::from(&string_repr[i..i+8]);
         for i in (1..8).rev() {
             eight_chars.insert(i, ' ');
         }
-        println!("▒ {} ▒", eight_chars);
+        println!("| {} |", eight_chars);
     }
     println!("{}", border);
 }
