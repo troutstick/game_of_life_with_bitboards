1b35f287ae68b386775b45ccba52b5064af41b0a
Justin Thein <jthein@berkeley.edu>
2020-05-25 23:07:31 -0700

    improving readability, adding consts for print function

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,62 @@
-const LEFT: u64 = 0x8080808080808080;
-const RIGHT: u64 = 0x0101010101010101;
-const TOP: u64 = 0x00000000000000FF;
-const BOTTOM: u64 = 0xFF00000000000000;
-const NLEFT: u64 = 0x7F7F7F7F7F7F7F7F; // !LEFT
-const NRIGHT: u64 = 0xFEFEFEFEFEFEFEFE; // !RIGHT
-
-struct GameOfLife {
-    state: u64,
-    bit1: u64,
-    bit2: u64,
-    bit3: u64,
-}
-
-impl GameOfLife {
-    fn build_game(starting_state: u64) -> GameOfLife {
-        GameOfLife {
-            state: starting_state,
-            bit1: 0,
-            bit2: 0,
-            bit3: 0,
-        }
-    }
-
-    fn step(&mut self) {
-        let c10 = self.state >> 8 | ((self.state & TOP) << 56);
-        let c12 = self.state << 8 | ((self.state & BOTTOM) >> 56);
-        let c00 = (c10 & NLEFT) << 1 | ((c10 & LEFT) >> 7);
-        let c01 = (self.state & NLEFT) << 1 | ((self.state & LEFT) >> 7);
-        let c02 = (c12 & NLEFT) << 1 | ((c12 & LEFT) >> 7);
-        let c20 = (c10 & NRIGHT) >> 1 | ((c10 & RIGHT) << 7);
-        let c21 = (self.state & NRIGHT) >> 1 | ((self.state & RIGHT) << 7);
-        let c22 = (c12 & NRIGHT) >> 1 | ((c12 & RIGHT) << 7);
-
-        self.bit1 = 0;
-        self.bit2 = 0;
-        self.bit3 = 0;
-
-        self.add(c00); self.add(c01); self.add(c02); self.add(c10);
-        self.add(c12); self.add(c20); self.add(c21); self.add(c22);
-
-        self.state = (self.state | self.bit1) & self.bit2 & !self.bit3;
-    }
-
-    fn add(&mut self, cxx: u64) {
-        let carry1 = self.bit1 & cxx;
-        let carry2 = self.bit2 & carry1;
-        self.bit1 ^= cxx;
-        self.bit2 ^= carry1;
-        self.bit3 ^= carry2;
-    }
-}
+const L: u64 = 0x8080808080808080;
+const R: u64 = 0x0101010101010101;
+const T: u64 = 0x00000000000000FF;
+const B: u64 = 0xFF00000000000000;
+const NL: u64 = 0x7F7F7F7F7F7F7F7F; // !LEFT
+const NR: u64 = 0xFEFEFEFEFEFEFEFE; // !RIGHT
 
 /// Reproduction of an 8x8 Conway's Game of Life in Rust (Browne and Tavener, 2012b).
 fn main() {
-    let mut game = GameOfLife::build_game(0x0000003808100000);
-    print_board(game.state);
+    let glider_state = 0x0000003808100000;
+    let mut game = GameOfLife::build_game(glider_state);
+    print_board(game.state());
     for i in 0..100 {
         game.step();
-        print_board(game.state);
+        print_board(game.state());
+    }
+}
+
+struct GameOfLife(u64, u64, u64, u64);
+
+impl GameOfLife {
+    fn build_game(starting_state: u64) -> GameOfLife {
+        GameOfLife(starting_state, 0, 0, 0)
+    }
+
+    /// Return the game's current state.
+    fn state(&self) -> u64 {
+        self.0
+    }
+
+    /// Step through the game once.
+    fn step(&mut self) {
+        let c10 = self.0        >> 8 | ((self.0 & T) << 56);
+        let c12 = self.0        << 8 | ((self.0 & B) >> 56);
+        let c00 = (c10    & NL) << 1 | ((c10    & L) >> 7);
+        let c01 = (self.0 & NL) << 1 | ((self.0 & L) >> 7);
+        let c02 = (c12    & NL) << 1 | ((c12    & L) >> 7);
+        let c20 = (c10    & NR) >> 1 | ((c10    & R) << 7);
+        let c21 = (self.0 & NR) >> 1 | ((self.0 & R) << 7);
+        let c22 = (c12    & NR) >> 1 | ((c12    & R) << 7);
+
+        self.1 = 0;
+        self.2 = 0;
+        self.3 = 0;
+
+        self.add(c00); self.add(c01);
+        self.add(c02); self.add(c10);
+        self.add(c12); self.add(c20);
+        self.add(c21); self.add(c22);
+
+        self.0 = (self.0 | self.1) & self.2 & !self.3;
+    }
+
+    fn add(&mut self, cxx: u64) {
+        let carry1 = self.1 & cxx;
+        let carry2 = self.2 & carry1;
+        self.1 ^= cxx;
+        self.2 ^= carry1;
+        self.3 ^= carry2;
     }
 }
 
@@ -82,9 +81,7 @@ fn main() {
 ///     a  b  c  d  e  f  g  h
 fn print_board(int: u64) {
     let mut string_repr = format!("{:064b}", int);
-    let files = "    a  b  c  d  e  f  g  h  ";
-    let border = "  +------------------------+";
-    println!("{}", border);
+    println!("{}", BORDER_STR);
 
     let mut counter = 8;
     for i in (0..64).step_by(8).rev() {
@@ -97,5 +94,8 @@ fn print_board(int: u64) {
         println!("{} | {} |", counter, eight_chars);
         counter -= 1;
     }
-    println!("{}\n{}", border, files);
+    println!("{}\n{}", BORDER_STR, FILES_STR);
 }
+
+const FILES_STR: &str = "    a  b  c  d  e  f  g  h  ";
+const BORDER_STR: &str = "  +------------------------+";
